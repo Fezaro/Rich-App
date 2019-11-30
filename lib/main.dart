0bb21107aa3168773ffa30b426f42d413cf34496
Max <mxmugendi@gmail.com>
2019-11-30 08:57:43 +0300

    Icons added for android and ios

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,16 @@ void main() => runApp(
       MaterialApp(
         home: Scaffold(
           appBar: AppBar(
-            title: Text('Rich App'),
+            title: Text('The Rich App'),
             centerTitle: true,
             backgroundColor: Colors.blueAccent[700],
           ),
           backgroundColor: Colors.indigoAccent[100],
+          body: Center(
+            child: Image(
+              image: AssetImage('images/Tanzanite.jpg'),
+            ),
+          ),
         ),
       ),
     );
